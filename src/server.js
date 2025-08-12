5b020920239f20bedbabd8419c8c5b19d27ab653
dixit <vasoyadixit992@gmail.com>
2025-08-12 17:58:33 +0530

    [fix]:cors error

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express"
 import "dotenv/config"
 import { connectDb } from "./lib/db.js"
 import dotenv from "dotenv"
+import cors from "cors"
 import authRoutes from "./routes/auth.route.js"
 import bookRoutes from "./routes/book.route.js"
 
 const app = express()
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000
